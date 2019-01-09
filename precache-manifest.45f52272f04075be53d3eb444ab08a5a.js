ed75800f2c5eb55cbf99b47aac3b1821a4728cb7
CasePloeg <case.ploeg@mail.utoronto.ca>
2019-01-09 17:08:00 -0500

    Updates

---
+++ precache-manifest.45f52272f04075be53d3eb444ab08a5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fb56de7f3bc67c4d74d",
+    "url": "/torontowaste/static/js/runtime~main.2fb56de7.js"
+  },
+  {
+    "revision": "1bffc49700628e99a350",
+    "url": "/torontowaste/static/js/main.1bffc497.chunk.js"
+  },
+  {
+    "revision": "07140812a98c93f90dd2",
+    "url": "/torontowaste/static/js/1.07140812.chunk.js"
+  },
+  {
+    "revision": "1bffc49700628e99a350",
+    "url": "/torontowaste/static/css/main.18f1bb44.chunk.css"
+  },
+  {
+    "revision": "dff3d63e223e757f825ffe39dec1974b",
+    "url": "/torontowaste/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7d235d54f98a63cdbe33652efca2f615.js"
+  "/torontowaste/precache-manifest.45f52272f04075be53d3eb444ab08a5a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/torontowaste/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
