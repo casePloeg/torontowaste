f33a9d727ce68fa53dcde9c069ee13411af754e0
CasePloeg <case.ploeg@mail.utoronto.ca>
2019-01-10 08:56:41 -0500

    Updated documenation to better explain methods and functionality

--- src/App.js
+++ src/App.js
@@ -7,28 +7,37 @@ import Favourites from "./Favourites";
 import Footer from "./Footer";
 
 class App extends Component {
+  /**
+   * Represents the logic behind the Toronto Waste App, serves as a container for the rest of the components
+   * @constructor
+   */
   constructor() {
     super();
+    // resultItems are the users search results
+    // favourites are items that the user chose to save
     this.state = {
       resultItems: [],
       favourites: []
     };
 
-    //this.method = this.method.bind(this);
+    //bind handle methods to this so they can be passed down as props to other components
     this.handleSearch = this.handleSearch.bind(this);
     this.clearSearch = this.clearSearch.bind(this);
     this.handleNewFavourite = this.handleNewFavourite.bind(this);
     this.handleRemoveFavourite = this.handleRemoveFavourite.bind(this);
   }
 
-  componentDidMount() {}
-
-  // this method handles the api call, will update the results list
+  /**
+   * Method meant to handle the api call to the toronto waste database. Given a keyword by the user, this method updates the resultItems state
+   * trash items that match with the key word.
+   * @param {string} keyword
+   */
   handleSearch(keyword) {
-    // create a regex using the keyword given the by user, spaces are required so that words contained in other words aren't matched. ie, rat => crate
+    // create a regex using the keyword given the by user this is used to match entries in the database with the given keyword
     const re = new RegExp("." + keyword + ".");
+    // init an array that will hold the matching entries
     let results = [];
-    // make an fetch api call on the toronto waste database
+    // make an fetch api call on the toronto waste database and retrieve the json file
     fetch(
       "https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000"
     )
@@ -37,6 +46,7 @@ class App extends Component {
       })
       .then(
         function(myJson) {
+          // the forEach method is used here instead of filter because if an item is matched, the special characters in the entry must be converted in order for the html to render properly
           myJson.forEach(trashItem => {
             // if the current trash item contains the keyword given by the user, add this item to the results items that will be displayed to the user
 
@@ -50,29 +60,39 @@ class App extends Component {
               trashItem.body = trashItem.body.replace(/&gt;/g, ">");
               trashItem.body = trashItem.body.replace(/&amp;/g, "&");
               trashItem.body = trashItem.body.replace(/&quot;/g, "");
-
+              // add the matched entry to the results
               results.push(trashItem);
             }
           });
+          // update the resultsItems state to the new matched entries
           this.setState(prevState => (prevState["resultItems"] = results));
         }.bind(this)
       );
   }
 
+  /**
+   * Method to reset the search results state item
+   */
   clearSearch() {
     // clear the result items
     this.setState(prevState => (prevState["resultItems"] = []));
   }
 
-  // this method handles the user requesting to add a favourite item
+  /**
+   * Method to handle the user requesting to add a waste item to the favourites list
+   * @param {object} item
+   */
   handleNewFavourite(item) {
     // append the given item which includes the title and body text to the favourites state
     this.setState(prevState => prevState["favourites"].push(item));
   }
 
-  // this method handles the user requesting to remove a favourited item
+  /**
+   * Method to handle the user requesting to remove a favourited item from their list
+   * @param {object} item
+   */
   handleRemoveFavourite(item) {
-    //remove the given id from the favourites state
+    //remove the given item from the favourites state, do not alter the state if the given item is not currently favourited
     this.setState(prevState => {
       const index = prevState["favourites"].findIndex(
         favItem => favItem.title === item.title
@@ -85,6 +105,9 @@ class App extends Component {
     });
   }
 
+  /**
+   * Method to render the app using react
+   */
   render() {
     return (
       <div>
--- src/ResultsList.js
+++ src/ResultsList.js
@@ -3,7 +3,7 @@ import TrashItem from "./TrashItem";
 
 class ResultsList extends Component {
   render() {
-    // create displayable trash item components out of results given throug props
+    // create displayable trash item components out of results given through props
     // an item is a favourite if the object consisting of it's item and body appears in the favourites list
     // titles are unique so they are given as the key. Ids are not used because they seem to be missing from some items in the database
     const resultItems = this.props.resultItems.map(item => (
